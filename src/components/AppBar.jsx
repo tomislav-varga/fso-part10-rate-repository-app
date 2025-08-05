3d03385de686e7a78980d6541f338b8ea4a9c61b
tomi <tomislav.varga@icloud.com>
2025-08-05 14:19:57 +0200

    solution exercise 10.6

--- src/components/AppBar.jsx
+++ src/components/AppBar.jsx
@@ -1,19 +1,26 @@
-import { View, StyleSheet } from 'react-native';
+import { View, StyleSheet, ScrollView } from 'react-native';
 import Constants from 'expo-constants';
+import theme from '../../theme';
 import AppBarTab from './AppBarTab';
 
 const styles = StyleSheet.create({
   container: {
     paddingTop: Constants.statusBarHeight,
-    // ...
+    backgroundColor: theme.appBar.background,
+    paddingLeft: 10,
   },
-  // ...
+  scrollView: {
+    flexDirection: 'row',
+  },
 });
 
 const AppBar = () => {
   return (
     <View style={styles.container}>
-      <AppBarTab />
+      <ScrollView horizontal showsHorizontalScrollIndicator={false} style={styles.scrollView}>
+        <AppBarTab to="/">Repositories</AppBarTab>
+        <AppBarTab to="/signin">Sign in</AppBarTab>
+      </ScrollView>
     </View>
   );
 };
---
+++ src/components/SignIn.jsx
@@ -0,0 +1,7 @@
+import Text from '../Text';
+
+const SignIn = () => {
+  return <Text fontSize="subheading" color="primary">The sign-in view</Text>;
+};
+
+export default SignIn;

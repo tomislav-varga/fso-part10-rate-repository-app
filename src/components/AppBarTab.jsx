24bcc8cb50d9f60594e9a7724226f54fc3ebe531
tomi <tomislav.varga@icloud.com>
2025-08-05 14:19:57 +0200

    solution exercise 10.6 and 10.7

--- src/components/AppBarTab.jsx
+++ src/components/AppBarTab.jsx
@@ -1,29 +1,22 @@
-import { View, StyleSheet, Pressable } from 'react-native';
-import Constants from 'expo-constants';
+import { StyleSheet, Pressable } from 'react-native';
+import { Link } from 'react-router-native';
 import theme from '../../theme';
 import Text from '../Text';
 
 const styles = StyleSheet.create({
-    container: {
-        paddingTop: Constants.statusBarHeight,
-        backgroundColor: theme.appBar.background,
-        padding: 12,
-    },
-    text: {
-        color: theme.appBar.textPrimary,
-        fontSize: theme.fontSizes.subheading,
-        fontFamily: theme.fonts.main,
-        fontWeight: theme.fontWeights.bold,
+    tab: {
+        paddingHorizontal: 15,
+        paddingVertical: 10,
     },
 });
 
-const AppBarTab = () => {
+const AppBarTab = ({ children, to }) => {
     return (
-        <View style={styles.container}>
-            <Pressable onPress={() => {}}>
-                <Text fontWeight="bold">Repositories</Text>
-            </Pressable>
-        </View>
+        <Link to={to} component={Pressable} style={styles.tab} >
+            <Text fontWeight="bold" style={{ color: theme.colors.textPrimary }}>
+                {children}
+            </Text>
+        </Link>
     );
 }
 
